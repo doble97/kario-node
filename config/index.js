6995613fd4c6748160acd3bacf0fc521b985adaf
Jorge Gonzalez <jorgegonzalez97d@gmail.com>
2022-04-22 12:31:03 +0200

    Configuracion bbdd

---
+++ config/index.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    DB_HOST: '127.0.0.1',
+    DB_USER: 'root',
+    DB_PASSWORD: 'alumno2021',
+    DB_DATABASE: 'Kario',
+    SECURITY_SALT: 'RseIr5rsLeZUc0gweKaHqRxC1-JBgXPX',
+    SECURITY_IV: "VEKTvuwZ3hw'bYPG"
+}
